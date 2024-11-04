10c313fdac2bc2b7d58f6db1f70f38e85930e36b
Andrei Markin <amarkin@google.com>
2024-11-04 12:38:55 +0400

    Commit for core

---
+++ libs/garf_core/tests/unit/test_report_fetcher.py
@@ -0,0 +1,70 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+
+from gaarf_core import (
+  api_clients,
+  parsers,
+  report,
+  report_fetcher,
+)
+
+
+class TestApiReportFetcher:
+  @pytest.fixture
+  def test_list_report_fetcher(self):
+    test_api_client = api_clients.FakeApiClient(results=[[1], [2], [3]])
+    return report_fetcher.ApiReportFetcher(
+      api_client=test_api_client, parser=parsers.ListParser
+    )
+
+  @pytest.fixture
+  def test_dict_report_fetcher(self):
+    test_api_client = api_clients.FakeApiClient(
+      results=[
+        {'column': {'name': 1}, 'other_column': 2},
+        {'column': {'name': 2}, 'other_column': 2},
+        {'column': {'name': 3}, 'other_column': 2},
+      ]
+    )
+    return report_fetcher.ApiReportFetcher(
+      api_client=test_api_client, parser=parsers.DictParser
+    )
+
+  def test_fetch_returns_correct_report_for_list_parser(
+    self, test_list_report_fetcher
+  ):
+    query = 'SELECT column as column_name FROM test'
+    test_report = test_list_report_fetcher.fetch(query, None)
+
+    expected_report = report.GaarfReport(
+      results=[[1], [2], [3]],
+      column_names=['column_name'],
+    )
+
+    assert test_report == expected_report
+
+  def test_fetch_returns_correct_report_for_dict_parser(
+    self, test_dict_report_fetcher
+  ):
+    query = 'SELECT column.name, other_column FROM test'
+    test_report = test_dict_report_fetcher.fetch(query, None)
+
+    expected_report = report.GaarfReport(
+      results=[[1, 2], [2, 2], [3, 2]],
+      column_names=['column_name', 'other_column'],
+    )
+
+    assert test_report == expected_report
