06a1ea317b1cb55c66d206824a723899c94080a4
Andrei Markin <amarkin@google.com>
2025-10-06 22:05:03 +0400

    [dbm] feat: add processing of a query

--- libs/community/google/bid-manager/garf_bid_manager/query_editor.py
+++ libs/community/google/bid-manager/garf_bid_manager/query_editor.py
@@ -13,8 +13,41 @@
 # limitations under the License.
 """Defines Bid Manager API specific query parser."""
 
+import re
+
 from garf_core import query_editor
+from typing_extensions import Self
 
 
 class BidManagerApiQuery(query_editor.QuerySpecification):
   """Query to Bid Manager API."""
+
+  def extract_resource_name(self) -> Self:
+    super().extract_resource_name()
+    self.query.resource_name = self.query.resource_name.upper()
+    return self
+
+  def extract_fields(self) -> Self:
+    super().extract_fields()
+    fields = []
+    for field in self.query.fields:
+      fields.append(_normalize_field(field))
+    self.query.fields = fields
+    return self
+
+  def extract_filters(self) -> Self:
+    super().extract_filters()
+    filters = []
+    for field in self.query.filters:
+      if field.lower().startswith('datarange'):
+        filters.append(field)
+      else:
+        filters.append(_normalize_field(field))
+    self.query.filters = filters
+    return self
+
+
+def _normalize_field(field: str) -> str:
+  if re.match('^(metric|filter)_*', field.lower(), re.IGNORECASE):
+    return field.upper()
+  return f'FILTER_{field.upper()}'
