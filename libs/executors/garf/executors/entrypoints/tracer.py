0b36f13c01e98e6758c61a84483bb79e1accf389
Andrei Markin <amarkin@google.com>
2026-02-04 11:37:50 +0400

    feat(executors): add export of metrics to Google Cloud

--- libs/executors/garf/executors/entrypoints/tracer.py
+++ libs/executors/garf/executors/entrypoints/tracer.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Opentelemetry initialization functions."""
+
 import os
 
 from opentelemetry import metrics, trace
@@ -25,7 +27,7 @@ from opentelemetry.sdk.metrics import MeterProvider
 from opentelemetry.sdk.metrics.export import (
   PeriodicExportingMetricReader,
 )
-from opentelemetry.sdk.resources import Resource
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
@@ -34,7 +36,7 @@ DEFAULT_SERVICE_NAME = 'garf'
 
 def initialize_tracer():
   resource = Resource.create(
-    {'service.name': os.getenv('OTLP_SERVICE_NAME', DEFAULT_SERVICE_NAME)}
+    {SERVICE_NAME: os.getenv('OTLP_SERVICE_NAME', DEFAULT_SERVICE_NAME)}
   )
 
   tracer_provider = TracerProvider(resource=resource)
@@ -50,7 +52,7 @@ def initialize_tracer():
         ) from e
 
       cloud_span_processor = BatchSpanProcessor(
-        CloudTraceSpanExporter(project_id=gcp_project_id)
+        CloudTraceSpanExporter(project_id=gcp_project_id, resource_regexp='*')
       )
       tracer_provider.add_span_processor(cloud_span_processor)
     else:
@@ -64,15 +66,30 @@ def initialize_tracer():
 
 def initialize_meter():
   resource = Resource.create(
-    {'service.name': os.getenv('OTLP_SERVICE_NAME', DEFAULT_SERVICE_NAME)}
+    {SERVICE_NAME: os.getenv('OTLP_SERVICE_NAME', DEFAULT_SERVICE_NAME)}
   )
   meter_provider = MeterProvider(resource=resource)
 
   if otel_endpoint := os.getenv('OTEL_EXPORTER_OTLP_ENDPOINT'):
-    otlp_metric_exporter = OTLPMetricExporter(
-      endpoint=f'{otel_endpoint}/v1/metrics'
-    )
-    metric_reader = PeriodicExportingMetricReader(otlp_metric_exporter)
+    if gcp_project_id := os.getenv('OTEL_EXPORTER_GCP_PROJECT_ID'):
+      try:
+        from opentelemetry.exporter.cloud_monitoring import (
+          CloudMonitoringMetricsExporter,
+        )
+      except ImportError as e:
+        raise ImportError(
+          'Please install garf-executors with GCP support '
+          '- `pip install garf-executors[gcp]`'
+        ) from e
+
+      metric_exporter = CloudMonitoringMetricsExporter(
+        project_id=gcp_project_id, resource_regexp='*'
+      )
+    else:
+      metric_exporter = OTLPMetricExporter(
+        endpoint=f'{otel_endpoint}/v1/metrics'
+      )
+    metric_reader = PeriodicExportingMetricReader(metric_exporter)
     meter_provider = MeterProvider(
       resource=resource, metric_readers=[metric_reader]
     )
