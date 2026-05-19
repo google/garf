7cbdd27281e8ad8a39617a8c6644bf8c4b3fc3f2
Andrei Markin <amarkin@google.com>
2026-05-20 01:21:13 +0400

    feat(executors): check required garf version is supplied in workflow

---
+++ libs/executors/tests/unit/test_init.py
@@ -0,0 +1,37 @@
+# Copyright 2026 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import re
+
+import garf.executors
+import pytest
+
+
+def test_validate_version_raises_error_on_lower_version():
+  test_version = '100000.10.4'
+  with pytest.raises(
+    garf.executors.exceptions.GarfExecutorError,
+    match=re.escape(
+      f'Garf version ({garf.executors.__version__}) is below required '
+      f'by workflow - {test_version}.'
+    ),
+  ):
+    garf.executors.validate_version(test_version)
+
+
+@pytest.mark.parametrize(
+  'test_version',
+  [None, '0.0.0', '0.100000.100000', '1.0.0', garf.executors.__version__],
+)
+def test_validate_version_returns_true(test_version):
+  assert garf.executors.validate_version(test_version)
