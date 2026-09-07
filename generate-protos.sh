520b70a3e3b3c1ecb060428b232237a7a03702ce
Andrei Markin <amarkin@google.com>
2026-09-07 12:18:43 +0400

    feat(go-sdk): initial commit for Garf Go SDK

--- generate-protos.sh
+++ generate-protos.sh
@@ -1,7 +1,13 @@
-garf() {
+python() {
 	python -m grpc_tools.protoc -I=protos/ \
 		--python_out=./libs/executors/garf/executors \
 		--grpc_python_out=./libs/executors/garf/executors \
 		protos/garf.proto
 }
-garf
+go() {
+	protoc --proto_path=protos/ \
+	--go_out=sdk/go/garf/ --go_opt=paths=source_relative \
+    --go-grpc_out=sdk/go/garf/ --go-grpc_opt=paths=source_relative \
+		protos/garf.proto
+}
+python
---
+++ sdk/go/README.md
@@ -0,0 +1,12 @@
+# Go SDK for garf server
+
+
+## Prerequisites
+
+* `garf` gRPC server running; expose as `GARF_ENDPOINT` variable.
+
+## Run
+
+```bash
+go run .
+```
