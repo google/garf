6de500123afed10d71dbcd1ca7884dcb5ab95d70
Andrei Markin <amarkin@google.com>
2026-09-16 15:05:02 +0400

    feat(rust-sdk): add support for reading workflow and config from a file

--- sdk/rust/build.rs
+++ sdk/rust/build.rs
@@ -1,15 +1,13 @@
 use std::io::Result;
 
 fn main() -> Result<()> {
-    let mut config = tonic_prost_build::configure();
-    let serializable_types = [".garf.FetcherInfo"];
-
-    for proto_type in serializable_types {
-        config =
-            config.type_attribute(proto_type, "#[derive(serde::Serialize)]")
-    }
-    config
-        .build_server(false) // .out_dir("src")
+    tonic_prost_build::configure()
+        .build_server(false)
+        // .out_dir("src")
+        .type_attribute(".", "#[derive(serde::Serialize, serde::Deserialize)]")
+        .field_attribute(".", "#[serde(default)]")
+        .extern_path(".google.protobuf.Struct", "::pbjson_types::Struct")
+        .extern_path(".google.protobuf.Value", "::pbjson_types::Value")
         .compile_protos(
             &["../../protos/garf.proto"],
             &["../../protos", "src"],
